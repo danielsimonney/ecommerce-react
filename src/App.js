3d2e45c40464a2fdb2da9b24d0070f8f23a57dc3
danielsimonney <daniel.simonney60@gmail.com>
2020-09-18 00:07:40 +0200

    begin auth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import Homepage from './pages/homepage/homepage.component.jsx'
+import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
+import Connexion from './pages/connexion/connexion'
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={Connexion} />
       </Switch>
     </div>
   );
---
+++ src/pages/connexion/connexion.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+import './connexion.scss';
+import SignIn from '../../components/sign-in/sign-in.component'
+
+const Connexion = () => (
+  <div className='connexion'>
+    <SignIn />
+  </div>
+);
+ 
+export default Connexion;
